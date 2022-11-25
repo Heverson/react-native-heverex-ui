69ad8f8c56a921ca13b0bb4683aaee8364ae92c9
Heverson <heversondamasceno@gmail.com>
2022-11-25 16:29:51 -0300

    add component BoxRounded

---
+++ src/components/BoxRounded/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export function BoxRounded() {
+  return (
+    <View
+      style={{
+        backgroundColor: '#e8e8e8',
+        width: '100%',
+        margin: 10,
+        padding: 8,
+      }}
+    >
+      <Text>BoxRounded</Text>
+    </View>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './Buttons/ButtonText';
 export * from './Buttons/ButtonTap';
 export * from './Input';
 export * from './Title';
+export * from './BoxRounded';
